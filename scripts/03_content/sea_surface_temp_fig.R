f9a138f997bec1a5bff6f5bb561cbafc0c22e0ee
jamtuttlebee <jamtuttlebee@gmail.com>
2025-09-04 10:16:27 -0400

    updated with plot(p)

--- scripts/03_content/sea_surface_temp_fig.R
+++ scripts/03_content/sea_surface_temp_fig.R
@@ -11,12 +11,14 @@ data("data_heatwaves")
 p <- ggplot(data = data_heatwaves,
             mapping = aes(x = year, y = temp_mean,
                           group = paste(fishery, eu_rnpa))) +
-  geom_line(size = 0.2, color = "red") +
+  geom_line(size = 0.1, color = "red") +
   labs(x = "Year",
        y = "Mean Temperature (C)",
        title = "Mean Temp. by Year",
        subtitle = "(It looks like the Grand Canyon)",
        caption = "Getting hotter...")
+# this makes sure it actually updates p and sends it to "plots", but still have to run both
+plot(p)
 
 # export my plot
 ggsave(plot = p,
